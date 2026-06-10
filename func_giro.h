083c34e21ba9ab4dce73cfa8d27182f95f52099b
carritoautogol <carrito.autogol@gmail.com>
2026-06-10 13:58:51 -0700

    Include Arduino.h in func_giro.h
    Added Arduino.h include for MPU functions.

--- func_giro.h
+++ func_giro.h
@@ -1,6 +1,8 @@
 #ifndef FUNC_GIRO_H
 #define FUNC_GIRO_H
 
+#include <Arduino.h>
+
 /* ===================== FUNCIONES DEL SENSOR MPU I2C =========================== */
 void mpuW(uint8_t r, uint8_t v);  // Función para escribir en el MPU
 int16_t mpuGz();  // Función para extraer datos físicos del MPU
