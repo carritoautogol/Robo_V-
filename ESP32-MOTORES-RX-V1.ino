5e4fb1c8813c2822392b2cc669cae82d0d2e98b3
carritoautogol <carrito.autogol@gmail.com>
2026-06-10 13:28:20 -0700

    Add files via upload

---
+++ ESP32-MOTORES-RX-V1.ino
@@ -0,0 +1,12 @@
+#include <Arduino.h> // Incluye las rutinas principales de la ESP32
+#include <Wire.h>    // Incluye la librería para leer el giroscopio por I2C
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ func_motor.cpp
@@ -0,0 +1,2 @@
+#include "func_motor.h"
+
