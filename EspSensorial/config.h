72a9237b59fb387d2be9d49f43e90b19e38dc5d6
carritoautogol <carrito.autogol@gmail.com>
2026-06-11 12:46:55 -0700

    Add configuration for sensor trigger and echo pins

---
+++ EspSensorial/config.h
@@ -0,0 +1,14 @@
+
+
+
+#define TRIG_F 13 // Frente
+#define ECHO_F 12
+
+#define TRIG_B 14 // Atrás
+#define ECHO_B 27
+
+#define TRIG_L 32 // Izquierda
+#define ECHO_L 33
+
+#define TRIG_R 22 // Derecha
+#define ECHO_R 23
