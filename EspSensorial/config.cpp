2079adaad3e53e9dd95d68a0615ec2eb092071d5
Helios Sanchez <helioselcrack@gmail.com>
2026-06-11 21:06:18 -0700

    Update config.cpp

--- EspSensorial/config.cpp
+++ EspSensorial/config.cpp
@@ -1,1 +1,10 @@
 #include "config.h"
+
+// ============================================================
+//  NAVEGACION — ULTRASONICOS — variables mutables
+// ============================================================
+
+int distFrente = 999;
+int distAtras  = 999;
+int distIzq    = 999;
+int distDer    = 999;
