a184e6d0212a6e41fe93136be6f44116ce49279a
Helios Sanchez <helioselcrack@gmail.com>
2026-06-11 21:06:14 -0700

    Refactor pin definitions to use constexpr

--- EspSensorial/config.h
+++ EspSensorial/config.h
@@ -2,28 +2,49 @@
 #define CONFIG_H
 
 // ============================================================
+//  COMUNICACIÓN — UART — Pines de comunicacion
+// ============================================================
+
+constexpr RX_PIN 26 
+constexpr TX_PIN 25 
+HardwareSerial Enlace(1); 
+
+// ============================================================
+//  MULTIPLEXOR — Pines de multiplexor
+// ============================================================
+
+constexpr int pinS0 = 19; 
+constexpr int pinS1 = 18; 
+constexpr int pinS2 = 17; 
+constexpr int pinS3 = 16; 
+constexpr int pinSIG = 4; 
+
+constexpr int totalSensores = 16; 
+constexpr float GRADOS_POR_SENSOR = 22.5; 
+
+// ============================================================
 //  ULTRASONICOS — Pines de ultrasonicos
 // ============================================================
 
-#define TRIG_F 13 // Frente
-#define ECHO_F 12
+constexpr TRIG_F 13 // Frente
+constexpr ECHO_F 12
 
-#define TRIG_B 14 // Atrás
-#define ECHO_B 27
+constexpr TRIG_B 14 // Atrás
+constexpr ECHO_B 27
 
-#define TRIG_L 32 // Izquierda
-#define ECHO_L 33
+constexpr TRIG_L 32 // Izquierda
+constexpr ECHO_L 33
 
-#define TRIG_R 22 // Derecha
-#define ECHO_R 23
+constexpr TRIG_R 22 // Derecha
+constexpr ECHO_R 23
 
 // ============================================================
-//  NAVEGACION — ULTRASONICOS — variables mutables
+//  NAVEGACION — ULTRASONICOS
 // ============================================================
 
-extern int distFrente = 999;
-extern int distAtras  = 999;
-extern int distIzq    = 999;
-extern int distDer    = 999;
+extern int distFrente;
+extern int distAtras;
+extern int distIzq;
+extern int distDer;
 
 #endif
