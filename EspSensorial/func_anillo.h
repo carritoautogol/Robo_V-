9d983c5ec3fe55b334d2f9ed82b859c7bf4b0ff1
carritoautogol <carrito.autogol@gmail.com>
2026-06-15 13:58:52 -0700

    Enhance function documentation in func_anillo.h
    Added documentation for functions ubicarPelota and fotorreceptoresActivos.

--- EspSensorial/func_anillo.h
+++ EspSensorial/func_anillo.h
@@ -1,11 +1,17 @@
 #ifndef FUNC_ANILLO_H
 #define FUNC_ANILLO_H
 
-/*	Cadena de fotorreceptores adyacentes encendidos mas larga para descartar 
-rebotes de luz en otros fotorreceptores */
+/**
+ * @brief Encuentra el bloque contiguo más largo de sensores activos
+ *        y calcula el ángulo mediante centroide.
+ * @return Número de sensores activos consecutivos más largo.
+ */
 int ubicarPelota();
 
-//Creo que el nombre es bastante intuitivo
+/**
+ * @brief Lee todos los canales del multiplexor y actualiza el vector 'activo[]'.
+ * @param totalActivos Referencia donde se almacena la cantidad de sensores activos.
+ */
 void fotorreceptoresActivos(int& totalActivos);
 
-#endif
+#endif // FUNC_ANILLO_H
