cd5eeb5e01c24d6021bf924af01df7dd87e04b93
carritoautogol <carrito.autogol@gmail.com>
2026-06-15 13:55:20 -0700

    Refactor motor function declarations in header

--- EspMotores/func_motor.h
+++ EspMotores/func_motor.h
@@ -1,23 +1,20 @@
 #ifndef FUNC_MOTOR_H
 #define FUNC_MOTOR_H
 
-void pwmInit(int pin);          // Inicializa el pin
-void pwmWrite(int pin, int d);  // Escribe la velocidad
+void pwmInit(int pin);
+void pwmWrite(int pin, int d);
 
-// Rutina que enciende una rueda específica
+// Control individual de cada rueda
 void rueda(int pwm, int in1, int in2, int v, bool inv);
-// Sub-rutinas rápidas para no escribir tanto en el loop
-void FL(int v);                               // Llama a la rueda Delantera Izquierda
-void FR(int v);                               // Llama a la rueda Delantera Derecha
-void RL(int v);                               // Llama a la rueda Trasera Izquierda
-void RR(int v);                               // Llama a la rueda Trasera Derecha
-void ruedas(int fl, int fr, int rl, int rr);  // Aplica valores a las 4 a la vez
-void detener();                               // Frena las 4 ruedas y reinicia la aceleración
-void girarEnSitio(int v);                     // Aplica fuerza contraria entre lados para pivotear sobre su eje
-// Sistema antideslizamiento y aceleración controlada
-void avanzarSuave(int objetivo, int corr);  // Recibe la velocidad máxima deseada y una corrección angular
+void FL(int v);
+void FR(int v);
+void RL(int v);
+void RR(int v);
+void ruedas(int fl, int fr, int rl, int rr);
 
+// Comandos de alto nivel
+void detener();
+void girarEnSitio(int v);
+void avanzarSuave(int objetivo, int corr);
 
-
-
-#endif
+#endif // FUNC_MOTOR_H
