b2c7770a74c6e53fa5ede2de9e5a9ab5893819bf
carritoautogol <carrito.autogol@gmail.com>
2026-06-15 13:59:11 -0700

    Update func_multiplexor.cpp

--- EspSensorial/func_multiplexor.cpp
+++ EspSensorial/func_multiplexor.cpp
@@ -1,10 +1,9 @@
-#include <Arduino.h>
+#include "func_multiplexor.h"
 #include "config.h"
 
-//Permite elegir mediante los 4 pines de comunicacion uno de los 16 fotorreceptores
 void seleccionarCanal(int canal) {
-  digitalWrite(pinS0, bitRead(canal, 0)); 
-  digitalWrite(pinS1, bitRead(canal, 1)); 
-  digitalWrite(pinS2, bitRead(canal, 2)); 
-  digitalWrite(pinS3, bitRead(canal, 3)); 
+  digitalWrite(pinS0, bitRead(canal, 0));
+  digitalWrite(pinS1, bitRead(canal, 1));
+  digitalWrite(pinS2, bitRead(canal, 2));
+  digitalWrite(pinS3, bitRead(canal, 3));
 }
