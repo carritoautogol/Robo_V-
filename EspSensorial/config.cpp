cae1fa79cbec7c429f8395e2d9a2221ab72e0a16
carritoautogol <carrito.autogol@gmail.com>
2026-06-19 20:32:52 -0700

    Update config.cpp

--- EspSensorial/config.cpp
+++ EspSensorial/config.cpp
@@ -3,9 +3,13 @@
 HardwareSerial Enlace(2);
 
 volatile int distFrente = 999;
-volatile int distAtras  = 999;
-volatile int distIzq    = 999;
-volatile int distDer    = 999;
+volatile int distAtras = 999;
+volatile int distIzq = 999;
+volatile int distDer = 999;
 
 volatile float angulo = -1.0;
 volatile bool activo[16];
+
+extern volatile float yaw = 0.0f;
+extern volatile float robotX = 0.0f;
+extern volatile float robotY = 0.0f;
