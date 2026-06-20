a44b4fd923b67ec44e8f124a65aa22e9c2e1c589
carritoautogol <carrito.autogol@gmail.com>
2026-06-19 20:34:37 -0700

    Update func_ultrasonicos.h

--- EspSensorial/func_ultrasonicos.h
+++ EspSensorial/func_ultrasonicos.h
@@ -1,22 +1,11 @@
 #ifndef FUNC_ULTRASONICOS_H
 #define FUNC_ULTRASONICOS_H
 
-/**
- * @brief Mide la distancia con un sensor HC-SR04.
- * @param trigPin Pin de trigger.
- * @param echoPin Pin de echo.
- * @return Distancia en centímetros, o 999 si no hay eco.
- */
-int medirDistancia(int trigPin, int echoPin);
+#include "config.h"
 
-/**
- * @brief Tarea de FreeRTOS que actualiza periódicamente las distancias.
- */
+int medirDistanciaInterrupt(int trig, SensorEcho& sensor);
 void TareaUltrasonicos(void * pvParameters);
-
-/**
- * @brief Inicia la tarea de ultrasonidos en el núcleo 0.
- */
 void iniciarUltrasonicos();
+void inicializarPinesUltrasonidos();
 
-#endif // FUNC_ULTRASONICOS_H
+#endif
