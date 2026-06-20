67280036dde16a29d6d46d3ee7a7a2490e4d7b87
carritoautogol <carrito.autogol@gmail.com>
2026-06-19 20:34:04 -0700

    Update func_multiplexor.cpp

--- EspSensorial/func_multiplexor.cpp
+++ EspSensorial/func_multiplexor.cpp
@@ -1,6 +1,11 @@
-#include "func_multiplexor.h"
+// func_multiplexor.cpp
+#include <Arduino.h>
 #include "config.h"
 
+/**
+ * @brief Selecciona un canal del CD74HC4067 escribiendo los bits en S0-S3.
+ * @param canal Número de canal (0-15).
+ */
 void seleccionarCanal(int canal) {
   digitalWrite(pinS0, bitRead(canal, 0));
   digitalWrite(pinS1, bitRead(canal, 1));
