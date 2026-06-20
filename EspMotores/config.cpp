c3675edf7e14f868490e0effcd170c4a3d3c63a4
carritoautogol <carrito.autogol@gmail.com>
2026-06-19 20:27:06 -0700

    Update config.cpp

--- EspMotores/config.cpp
+++ EspMotores/config.cpp
@@ -1,37 +1,36 @@
-  #include "config.h"
+#include "config.h"
 
-  // ====================== COMUNICACIÓN UART ======================
-  HardwareSerial Enlace(2);   // Serial2 para comunicación con el anillo
-  char uartBuffer[64];        // Buffer de recepción
-  volatile int bufIndex = 0;
-  volatile unsigned long ultimoDato = 0;
+volatile int velAvanceActual = 0;
+volatile unsigned long tSenalEstable = 0;
+volatile float Correccion = 0.0f;
 
-  // ====================== VARIABLES DE GIROSCOPIO ======================
-  volatile float yaw = 0.0f;
-  volatile float gyroZoffset = 0.0f;
-  volatile unsigned long tPrev = 0;
+HardwareSerial Enlace(2);
+volatile float anguloIR = -1.0f;
+volatile int estadoIR = 0;
+volatile int nIR = 0;
+volatile unsigned long ultimoDato = 0;
 
-  // ====================== VARIABLES DEL ANILLO ======================
-  volatile float anguloIR = -1.0f;
-  volatile int estadoIR = 0;
-  volatile int nIR = 0;
+volatile float yaw = 0.0f;
+volatile float gyroZoffset = 0.0f;
+volatile unsigned long tPrev = 0;
 
-  // ====================== VARIABLES DE ULTRASONIDOS ======================
-  volatile int distFrente = 999;
-  volatile int distAtras  = 999;
-  volatile int distIzq    = 999;
-  volatile int distDer    = 999;
+volatile EstadoRobot estadoActual = ESPERANDO_PELOTA;
+volatile unsigned long tFrenoIniciado = 0;
+volatile unsigned long tUltimaVezPelota = 0;
+volatile int pasoBusqueda = 0;
+volatile unsigned long tBusqueda = 0;
+volatile bool pelotaPerdidaReciente = false;
 
-  // ====================== VARIABLES DE MOTORES ======================
-  volatile int velAvanceActual = 0;
-  volatile unsigned long tSenalEstable = 0;
+volatile int distFrente = 999;
+volatile int distAtras = 999;
+volatile int distIzq = 999;
+volatile int distDer = 999;
+String recepVecinos;
 
-  // ====================== VARIABLES DE ESTADO ======================
-  volatile EstadoRobot estadoActual = ESPERANDO_PELOTA;
-  volatile unsigned long tFrenoIniciado = 0;
-  volatile unsigned long tUltimaVezPelota = 0;
+//Localizacion
+extern volatile float distSeguridad = 30.0f;
+volatile float posicionRobotX = 0.0f; //RECIBIDOS DEL UART tanto X como Y
+volatile float posicionRobotY = 0.0f;
 
-  volatile int pasoBusqueda = 0;
-  volatile unsigned long tBusqueda = 0;
-  volatile bool pelotaPerdidaReciente = false;
-  String recepVecinos;
+WebServer server(80);
+WiFiClient client;
