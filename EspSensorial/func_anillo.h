f6fdca6d457bec440554434a4cd9d23affcf4f3d
carritoautogol <carrito.autogol@gmail.com>
2026-06-19 20:33:28 -0700

    Update func_anillo.h

--- EspSensorial/func_anillo.h
+++ EspSensorial/func_anillo.h
@@ -1,17 +1,8 @@
+// func_anillo.h
 #ifndef FUNC_ANILLO_H
 #define FUNC_ANILLO_H
 
-/**
- * @brief Encuentra el bloque contiguo más largo de sensores activos
- *        y calcula el ángulo mediante centroide.
- * @return Número de sensores activos consecutivos más largo.
- */
-int ubicarPelota();
+int ubicarPelota();                     // Encuentra la cadena más larga y calcula el centroide
+void fotorreceptoresActivos(int& totalActivos); // Lee todos los sensores
 
-/**
- * @brief Lee todos los canales del multiplexor y actualiza el vector 'activo[]'.
- * @param totalActivos Referencia donde se almacena la cantidad de sensores activos.
- */
-void fotorreceptoresActivos(int& totalActivos);
-
-#endif // FUNC_ANILLO_H
+#endif
